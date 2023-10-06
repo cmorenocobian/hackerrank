200ae0583136bccdbc86823dd864be9dff4fc0d3
Carlos Moreno <48675342+cmorenocobian@users.noreply.github.com>
2023-10-06 08:34:45 +0200

    Update app_slicer.py

--- app_slicer.py
+++ app_slicer.py
@@ -13,25 +13,29 @@ data = pd.DataFrame({
 })
 
 # Set app title
-st.title("Enhanced Interactive Streamlit App")
+st.title("Interactive Streamlit App")
 
 # Date slicer
 start_date = st.date_input("Start Date", min_value=data['Date'].min(), max_value=data['Date'].max())
 end_date = st.date_input("End Date", min_value=data['Date'].min(), max_value=data['Date'].max())
 
-# Filter data based on selected date range
+# Convert the selected date inputs to datetime if needed
+start_date = pd.to_datetime(start_date)
+end_date = pd.to_datetime(end_date)
+
+# Filter data based on selected dates
 filtered_data = data[(data['Date'] >= start_date) & (data['Date'] <= end_date)]
 
-# Line plot
-st.subheader("Line Plot")
+# Line plot with multiple data series
+st.subheader("Line Plot with Multiple Data Series")
 st.line_chart(filtered_data.set_index('Date')[['Value1', 'Value2', 'Value3']])
 
-# Bar chart
-st.subheader("Bar Chart")
+# Bar chart with multiple data series
+st.subheader("Bar Chart with Multiple Data Series")
 st.bar_chart(filtered_data.set_index('Date')[['Value1', 'Value2', 'Value3']])
 
-# Histogram
-st.subheader("Histogram")
+# Histogram with multiple data series
+st.subheader("Histogram with Multiple Data Series")
 sns.histplot(data=filtered_data, x='Value1', bins=20, kde=True, label='Value1', color='blue')
 sns.histplot(data=filtered_data, x='Value2', bins=20, kde=True, label='Value2', color='green')
 sns.histplot(data=filtered_data, x='Value3', bins=20, kde=True, label='Value3', color='red')
@@ -40,8 +44,8 @@ plt.ylabel('Frequency')
 plt.legend()
 st.pyplot()
 
-# Summary statistics
-st.subheader("Summary Statistics")
-st.write(filtered_data.describe())
+# Summary statistics for multiple data series
+st.subheader("Summary Statistics for Multiple Data Series")
+st.write(filtered_data[['Value1', 'Value2', 'Value3']].describe())
 
 # You can add more plots and content as needed
